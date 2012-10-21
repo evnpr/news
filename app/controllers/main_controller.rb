1ad18c4b3fa53c53b089c5fe1f84a4b91b74e4cb
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-21 15:24:22 +0000

    evnpr just edit /libranews/app/controllers/main_controller.rb

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,22 @@
 class MainController < ApplicationController
   def index
   end
+  
+  def love
+  
+  end
+  
+  def hate
+  
+  end
+  
+  def pro
+  
+  end
+  
+  def kontra
+  
+  end
+  
 end
+
